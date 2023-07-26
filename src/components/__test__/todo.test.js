d3e42df182dea296c7dec8208c9821a8038861cb
reoko <johnmompil@gmail.com>
2023-07-26 13:33:34 +0300

    Update todo.test.js: add cleanup() function

--- src/components/__test__/todo.test.js
+++ src/components/__test__/todo.test.js
@@ -1,6 +1,10 @@
 import { render, screen, cleanup } from '@testing-library/react';
 import Todo from '../todo';
 
+afterEach(() => {
+    cleanup();
+});
+
 test('Should render non-completed todo', () => {
     const todo = {id: 1, title: 'wash dishes', completed: false};
     render(<Todo todo={todo} />);
