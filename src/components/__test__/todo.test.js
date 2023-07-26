d4c91373668fc7b17683eb84bf4470613af6aa9b
reoko <johnmompil@gmail.com>
2023-07-26 15:17:15 +0300

    Created snapshot test function

--- src/components/__test__/todo.test.js
+++ src/components/__test__/todo.test.js
@@ -1,4 +1,5 @@
 import { render, screen, cleanup } from '@testing-library/react';
+import renderer from 'react-test-renderer';
 import Todo from '../todo';
 
 afterEach(() => {
@@ -21,7 +22,7 @@ test('Should render non-completed todo', () => {
 test('Should render completed todo', () => {
     const todo = {id: 2, title: 'wash car', completed: true};
     render(<Todo todo={todo} />);
-
+    
     const todoElement = screen.getByTestId('todo-2');
     const strikeElement = todoElement.querySelector('strike');
 
@@ -29,3 +30,12 @@ test('Should render completed todo', () => {
     expect(todoElement).toBeInTheDocument();
     expect(todoElement).toHaveTextContent('wash car');
 });
+
+test('matches snaphot', () => {
+    const todo = {id: 1, title: 'wash dishes', completed: false};
+    const tree = renderer.create(<Todo todo={todo} />).toJSON;
+    expect(tree).toMatchSnapshot();
+    //console.log(tree);
+});
+
+//kentcdodds/react-testing-library
