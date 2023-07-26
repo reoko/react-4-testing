14aacd7df49f11922dbfb87ffc48844ad38480af
reoko <johnmompil@gmail.com>
2023-07-26 13:28:14 +0300

    Update App.js, todo.js and todo.test.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,18 @@ import './App.css';
 import Todo from './components/todo';
 
 function App() {
+
+  const todos = [
+    {id: 1, title: 'wash dishes', completed: false},
+    {id: 2, title: 'make dinner', completed: true}
+  ];
+
   return (
     <div className="App">
-      <Todo />
+      { todos.map((todo) => {
+          return (<Todo todo={todo} />)
+        })
+      }
     </div>
   );
 }
--- src/components/__test__/todo.test.js
+++ src/components/__test__/todo.test.js
@@ -1,3 +1,18 @@
-test('test', () => {
-    expect(true).toBe(true);
+import { render, screen, cleanup } from '@testing-library/react';
+import Todo from '../todo';
+
+test('Should render non-completed todo', () => {
+    const todo = {id: 1, title: 'wash dishes', completed: false};
+    render(<Todo todo={todo} />);
+    const todoElement = screen.getByTestId('todo-1');
+    expect(todoElement).toBeInTheDocument();
+    expect(todoElement).toHaveTextContent('wash dishes');
+});
+
+test('Should render completed todo', () => {
+    const todo = {id: 2, title: 'wash car', completed: true};
+    render(<Todo todo={todo} />);
+    const todoElement = screen.getByTestId('todo-2');
+    expect(todoElement).toBeInTheDocument();
+    expect(todoElement).toHaveTextContent('wash car');
 });
